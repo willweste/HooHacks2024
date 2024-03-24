eb5fc5e6d147a3cbee069e60810a00df148bfa45
WillWeste <willweste74@gmail.com>
2024-03-23 22:06:10 -0400

    local fully functional facial authentication

--- app/models.py
+++ app/models.py
@@ -6,7 +6,6 @@ class User(db.Model):
     username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
-    image_data = db.Column(db.LargeBinary)  # Changed to store binary data
 
     def __repr__(self):
         return f"<User {self.username}>"
